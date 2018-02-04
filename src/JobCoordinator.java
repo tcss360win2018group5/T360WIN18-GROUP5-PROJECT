cd09b69ff553b15b181e3fe9b7846a72647912d3
Tenma Rollins <tenmarollins@gmail.com>
2018-02-03 21:13:44 -0800

    ! refactored the coordinators so they can be instantiated (since they have fields that can be mutated among other things) ! changed user interface to instantiate necessary coordinators ! relocated date calculation to JobCoordinator class for now, until needed by another class

--- src/JobCoordinator.java
+++ src/JobCoordinator.java
@@ -1,53 +1,81 @@
 import java.util.ArrayList;
-
-import util.CalendarCalculator;
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
 
 public class JobCoordinator {
-	private final int MAXIMUM_JOBS = 20;
-	private final int MAXIMUM_JOB_LENGTH = 3; // days
-	private final int MAXIMUM_DAYS_FROM_TODAY = 75;
-	
-	private static ArrayList<Job> myPendingJobList = new ArrayList<Job>();
-	private static ArrayList<Job> myFinishedJobList = new ArrayList<Job>();
-	/**
-	 * Adds a job to my list of jobs
-	 * @param theJob that needs to be added
-	 */
-	public void addJob(final Job theJob) {
-	    if (myPendingJobList.contains(theJob)) {
-	        // warning, job already exists
-	    } else if (myPendingJobList.size() >= MAXIMUM_JOBS) {
-	        // warning, pending jobs at maximum
-	    } else if (theJob.getJobLength() > MAXIMUM_JOB_LENGTH) {
-	        // warning, job exceeds maximum job length
-	    } else if (CalendarCalculator.getDifferenceInDays(SystemCoordinator.getCurrentDate(),
-	                                                          theJob.getStartDate()) 
-	                    > MAXIMUM_DAYS_FROM_TODAY) {
-	        // warning, job is further than 75 days away
-	    } else {
-	        myPendingJobList.add(theJob);
-	    }
-	}
-	
-	public void applyToJob(Job theJob) {
+    private final int MAXIMUM_JOBS = 20; // 20 pending jobs max
+    private final int MAXIMUM_JOB_LENGTH = 3; // days
+    private final int MAXIMUM_DAYS_FROM_TODAY = 75; // 3 month away maximum
+    
+    private final ArrayList<Job> myPendingJobList;
+    private final ArrayList<Job> myFinishedJobList;
+    
+    private static GregorianCalendar myCurrentDate = new GregorianCalendar();
+    
+    public JobCoordinator() {
+        myPendingJobList = new ArrayList<Job>();
+        myFinishedJobList = new ArrayList<Job>();
+        
+    }
+    /**
+     * Adds a job to my list of jobs
+     * @param theJob that needs to be added
+     */
+    public void addPendingJob(final Job theJob) {
+        if (myPendingJobList.contains(theJob)) {
+            // warning, job already exists
+        } else if (theJob.getJobLength() > MAXIMUM_JOB_LENGTH) {
+            // warning, job exceeds maximum job length
+        } else if (getDifferenceInDays(JobCoordinator.myCurrentDate,
+                                                              theJob.getStartDate()) 
+                        > MAXIMUM_DAYS_FROM_TODAY) {
+            // warning, job is further than 75 days away
+        } else {
+            myPendingJobList.add(theJob);
+        }
+    }
+    
+    public void addFinishedJob(final Job theJob) {
+        if (JobCoordinator.myCurrentDate.compareTo(theJob.getEndDate()) < 0) {
+            // impossible for job to be finished, warning
+        } else {
+            myFinishedJobList.add(theJob);
+        }
+    }
+    
+    public void applyToJob(Job theJob) {
 
-	}
-	
-	
-	
-	/**
-	 * I think this function may be tweaked - Jon
-	 * @return the list of jobs
-	 */
-	@SuppressWarnings("unchecked")
+    }
+    
+    
+    
+    /**
+     * I think this function may be tweaked - Jon
+     * @return the list of jobs
+     */
+    @SuppressWarnings("unchecked")
     public ArrayList<Job> getPendingJobs() {
-		return (ArrayList<Job>) myPendingJobList.clone();
-	}
-	
+        return (ArrayList<Job>) myPendingJobList.clone();
+    }
+    
     @SuppressWarnings("unchecked")
     public ArrayList<Job> getFinshedJobs() {
         return (ArrayList<Job>) myFinishedJobList.clone();
     }
+    
+    
+    // testers
+    // can only potentially add a job if there are less than 20 pending jobss
+    public boolean canAddJob() {
+        return myPendingJobList.size() < MAXIMUM_JOBS;
+    }
 
-
+    
+    // private methods 
+    private int getDifferenceInDays(GregorianCalendar theFirstDate, 
+                                          GregorianCalendar theSecondDate) {
+        long milliTime = theFirstDate.getTimeInMillis() - theSecondDate.getTimeInMillis();
+        TimeUnit.MILLISECONDS.convert(milliTime, TimeUnit.DAYS);
+        return (int) milliTime;
+    }   
 }
--- src/SystemCoordinator.java
+++ src/SystemCoordinator.java
@@ -1,16 +1,15 @@
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 
 public class SystemCoordinator {
+	private final ArrayList<String> myUsers;
 	
-	//Should be some kind of date
-	private static GregorianCalendar myCurrentDate = new GregorianCalendar();
+	public SystemCoordinator() {
+	    myUsers = new ArrayList<String>();
+	}
 	
-	private static ArrayList<String> myUsers = new ArrayList<String>();
-	
-	protected static boolean signIn(String theUsername) {
+	protected boolean signIn(String theUsername) {
 		boolean isUser = false;
-		for (String s: myUsers) {
+		for (String s: this.myUsers) {
 			if (s.equals(theUsername)) {
 				isUser = true;
 			}
@@ -18,8 +17,8 @@ public class SystemCoordinator {
 		return isUser;
 	}
 	
-	public static GregorianCalendar getCurrentDate() {
-	    return myCurrentDate;
+	@SuppressWarnings("unchecked")
+    protected ArrayList<String> getUsers() {
+	    return (ArrayList<String>) myUsers.clone();
 	}
-
 }
