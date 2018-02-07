b0f4f68baa519b488e2bd47aabb0e0596ccf9766
Tenma Rollins <tenmarollins@gmail.com>
2018-02-07 12:42:51 -0800

    test

---
+++ src/view/UIControls.java
@@ -0,0 +1,16 @@
+package view;
+
+public enum UIControls {
+    // user commands
+    YES,
+    NO,
+    EXIT,
+    LOGIN,
+    LOGOUT,
+    SIGNUP,
+    VIEWJOBS,
+    // volunteer commands
+    APPLYTO,
+    // park manager commands
+    SUBMITNEWJOB;
+}
